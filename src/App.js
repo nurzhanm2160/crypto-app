32b9cea25adfe5936ac3b95e93427fa5116d3701
nurzhanm2160 <nurzhan.murzakhmet@gmail.com>
2022-12-09 22:27:40 +0600

    basic layout for App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom'
 import {Layout, Typography, Space} from "antd";
-import { Navbar } from "./components";
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from "./components";
 
 import './App.css'
 
@@ -12,10 +12,28 @@ const App = () => {
                 <Navbar />
             </div>
             <div className="main">
-
-            </div>
-            <div className="footer">
-
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route path="/" element={<Homepage />} />
+                            <Route path="/exchanges" element={<Exchanges />} />
+                            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                            <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+                            <Route path="/news" element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
+                <div className="footer">
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Cryptoverse <br/>
+                        All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
             </div>
         </div>
     );
