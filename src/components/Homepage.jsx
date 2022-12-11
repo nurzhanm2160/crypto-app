cd6c952c58bcb38cdd6b1e3a8fdab4d345bbb651
nurzhanm2160 <nurzhan.murzakhmet@gmail.com>
2022-12-11 22:12:06 +0600

    Homepage: millify statistic values, layout

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,7 @@ import {Typography, Row, Col, Statistic} from "antd";
 import {Link} from 'react-router-dom';
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import {Cryptocurrencies, News} from "../components";
 
 const {Title} = Typography
 
@@ -18,22 +19,39 @@ const Homepage = () => {
             </Title>
             <Row>
                 <Col span={12}>
-                    <Statistic title="Total Cryptocurrencies" value={globalStats.total}/>
+                    <Statistic title="Total Cryptocurrencies" value={globalStats?.total}/>
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total Exchanges" value={5}/>
+                    <Statistic title="Total Exchanges" value={millify(globalStats?.totalExchanges)}/>
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total Market Cap" value={5}/>
+                    <Statistic title="Total Market Cap" value={millify(globalStats?.totalMarketCap)}/>
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total 24h volume" value={5}/>
+                    <Statistic title="Total 24h volume" value={millify(globalStats?.total24hVolume)}/>
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total Markets" value={5}/>
+                    <Statistic title="Total Markets" value={millify(globalStats?.totalMarkets)}/>
                 </Col>
-
             </Row>
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">
+                    Top 10 Cryptocurrencies in the world
+                </Title>
+                <Title level={3} className="show-more">
+                    <Link to="/cryptocurrencies">Show More</Link>
+                </Title>
+            </div>
+            <Cryptocurrencies simplified />
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">
+                    Top 10 Cryptocurrencies in the world
+                </Title>
+                <Title level={3} className="show-more">
+                    <Link to="/news">Latest Crypto News</Link>
+                </Title>
+            </div>
+            <News simplified />
         </>
     );
 };
